a546a7dfdc1e9b3319189843bf4a39e29c9f3467
Harish Patel <harish@logicloop.io>
2022-09-26 16:49:07 +0530

    chanegs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  getUrl: "https://public.connectnow.org.uk",
+  getUrl: "https://public.connectnow.org.uk/",
   postUrl: "https://webhook.site/#!/19f6bcab-6fc0-4514-92d2-90ccaf4e7e19"
 };
